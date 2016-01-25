790bbffa388e7f7597a18ff69eb4257523aebc11
RolfBates <rnbates@umich.edu>
2016-01-25 17:20:54 -0500

    fixed buttons, fixed signin
    fixed the styling of the login buttons
    fixed the handling of the username data, so that it is present in all
    signins

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ var app = angular.module('app', ['keys']);
 // create the controller and inject Angular's $scope
 app
 .controller('main', ['$scope', '$rootScope', 'ParseSvc', function($scope, $rootScope, ParseSvc){
-   $scope.username = ParseSvc.getUsername();
+  $scope.username = $rootScope.username;
+  $rootScope.$on('new username', function(event, data){
+    $scope.username= data;
+    $scope.$apply();
+  });
 }])
 .controller('userlist', ['$scope', 'ParseSvc', function($scope, ParseSvc){
   $scope.users = [];
   $scope.sucessCallback = function(results) {
     for (i = 0; i < results.length; ++i) {
-       $scope.users.push(results[i].getUsername());
+      $scope.users.push(results[i].getUsername());
     } 
     $scope.$apply();
     console.log($scope.users);
@@ -20,140 +24,151 @@ app
   ParseSvc.getUsers($scope.sucessCallback);
 }])
 .controller('print', ['$scope', function($scope){
-   $scope.show_content = false;
-   $scope.printedText = "";
-   
-   $scope.print = function() {
-      $scope.show_content = !$scope.show_content;
-      if($scope.show_content)
-         $scope.printedText = "Hello World";
-      else
-         $scope.printedText = "";
-   }
+  $scope.show_content = false;
+  $scope.printedText = "";
+
+  $scope.print = function() {
+    $scope.show_content = !$scope.show_content;
+    if($scope.show_content)
+      $scope.printedText = "Hello World";
+    else
+      $scope.printedText = "";
+  }
 }])
 
 .controller('login', ['$scope', '$rootScope','ParseSvc', function($scope, $rootScope, ParseSvc){
-   $scope.user = {
-      username: null,
-      password: null,
-   };
-   if(ParseSvc.isRegistered) {
-      $rootScope.username = ParseSvc.getUsername();
+  //callback function to set global username on login sucess
+  //This is neccessary because JavaScript runs sychronously
+  //callback functions are one way to preserve asynchronicity
+  var loginCallback = function () {
+    $rootScope.$broadcast('new username', ParseSvc.getUsername());
   }
-   $scope.login = function () {
-      ParseSvc.login($scope.user)
-   }
-   
+  $scope.user = {
+    username: null,
+    password: null,
+  };
+  if(ParseSvc.isRegistered) {
+    $rootScope.username = ParseSvc.getUsername();
+  }
+  $scope.login = function () {
+    ParseSvc.login($scope.user, loginCallback)
+  }
+
 }])
 .controller('signUp', ['$scope','$rootScope','ParseSvc', function($scope, $rootScope, ParseSvc){
-   $scope.user = {
-      username: null,
-      password: null,
-      email: null,
-      firstName: null,
-      lastName: null
-   };
-   if(ParseSvc.isRegistered) {
-      $rootScope.username = ParseSvc.getUsername();
-   }
-   $scope.signUp = function () {
-      ParseSvc.signUp($scope.user)
-   }
-   
+  var signupCallback = function () {
+    $rootScope.$broadcast('new username', ParseSvc.getUsername());
+  }
+  $scope.user = {
+    username: null,
+    password: null,
+    email: null,
+    firstName: null,
+    lastName: null
+  };
+  if(ParseSvc.isRegistered) {
+    $rootScope.username = ParseSvc.getUsername();
+  }
+  $scope.signUp = function () {
+    ParseSvc.signUp($scope.user, signupCallback);
+  }
+
 }])
 .controller('reset', ['$scope','ParseSvc', function($scope, ParseSvc){
-   $scope.email = null;
-   $scope.resetPassword = function () {
-      ParseSvc.resetPassword()
-   }
-   
+  $scope.email = null;
+  $scope.resetPassword = function () {
+    ParseSvc.resetPassword($scope.email)
+  }
+
 }])
 .controller('logout', ['$scope','ParseSvc', function($scope, ParseSvc){
-   $scope.logout = function () {
-      ParseSvc.logout()
-   }
-   
+  $scope.logout = function () {
+    ParseSvc.logout()
+  }
+
 }])
 .factory('ParseSvc', ['$http', 'KeySvc', function($http, KeySvc) {
 
-   key1 = KeySvc.key1;
-   key2 = KeySvc.key2;
-   Parse.initialize(key1, key2);
-   var isRegistered;
+  key1 = KeySvc.key1;
+  key2 = KeySvc.key2;
+  Parse.initialize(key1, key2);
+  var isRegistered;
 
-   var user = Parse.User.current();
-   if (user) {
-      isRegistered = true;
-      // do stuff with the user
-   } else {
-     isRegistered = false;
-     user = new Parse.User();
+  var user = Parse.User.current();
+  if (user) {
+    isRegistered = true;
+    // do stuff with the user
+  } else {
+    isRegistered = false;
+    user = new Parse.User();
   }
 
-   return {
-   isRegistered: isRegistered,
-   getUsername: function() {
+  return {
+    isRegistered: isRegistered,
+    getUsername: function() {
       return user.get('username');
-   },
-   login: function(_user) {
+    },
+    login: function(_user, successCallback) {
       Parse.User.logIn(_user.username, _user.password, {
-         success: function(_user) {
-            user = _user;
-            console.log(user.get('username'));
-            // Do stuff after successful login.
-            alert('Logged in as ' + user.get('username'));
-         },
-         error: function(user, error) {
-            // The login failed. Check error to see why
-            console.log("Error: " + error.code + " " + error.message);
-            alert('Login failed');
-         }
+        success: function(_user) {
+          user = _user;
+          console.log(user.get('username'));
+          // Do stuff after successful login.
+          alert('Logged in as ' + user.get('username'));
+          successCallback();
+        },
+        error: function(user, error) {
+          // The login failed. Check error to see why
+          console.log("Error: " + error.code + " " + error.message);
+          alert('Login failed');
+        }
       });
-   },
-   signUp: function(_user) {
+    },
+    signUp: function(_user, successCallback) {
       user.set("username", _user.username.toLowerCase());
       user.set("email", _user.email);
       user.set("password", _user.password);
       user.set("firstName", _user.firstName);
       user.set("lastName", _user.lastName);
       user.signUp(null, {
-         success: function(user) {
-            // Hooray! Let them use the app now.
-            alert('Signed up successfully')
-         },
-         error: function(user, error) {
-            // Show the error message somewhere and let the user try again.
-            alert('There was an error. See the log to ge the message')
+        success: function(user) {
+          // Hooray! Let them use the app now.
+          alert('Signed up successfully')
+          successCallback();
+        },
+        error: function(user, error) {
+          // Show the error message somewhere and let the user try again.
+          alert('There was an error. See the log to ge the message')
             console.log("Error: " + error.code + " " + error.message);
-       }
-    });
-   },
-   resetPassword: function(email) {
-      Parse.User.requestPasswordReset("email", {
-         success: function() {
-         // Password reset request was sent successfully
-         alert("password reset link sent")
-         },
-         error: function(error) {
-            // Show the error message somewhere
-         alert("Error: " + error.code + " " + error.message);
-         }
+        }
       });
-   },
-   logout: function() {
+    },
+    resetPassword: function(email) {
+      Parse.User.requestPasswordReset(email, {
+        success: function() {
+          // Password reset request was sent successfully
+          alert("password reset link sent")
+        },
+        error: function(error) {
+          // Show the error message somewhere
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    },
+    logout: function() {
       Parse.User.logOut();
       console.log('logged out');
       alert('logged out');
       var currentUser = Parse.User.current(); 
       user = Parse.User.current(); 
-   },
-   getUsers: function(sucessCallback) {
+    },
+    getUsers: function(sucessCallback) {
       var user_query = new Parse.Query(Parse.User);
       user_query.select("username");
       user_query.find().then(function(results) {
-          sucessCallback(results);
+        sucessCallback(results);
       });
-   }
-};
+    }
+  };
 }])
 ;
